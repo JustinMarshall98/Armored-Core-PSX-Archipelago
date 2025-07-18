4f882e1a6e222a14f533d58bc60383a0bb6064bc
JustinMarshall98 <powtatow@gmail.com>
2025-07-18 14:24:07 -0400

    Add item definitions for missions and victory

---
+++ items.py
@@ -0,0 +1,28 @@
+import typing
+
+from BaseClasses import Item, ItemClassification
+from .utils import Constants
+from .mission import Mission, all_missions, name_to_mission
+
+item_id_to_item_name: typing.Dict[int, str] = {}
+
+# Mission unlock item ID's are the mission ID + mission completion offset
+for mission in all_missions:
+    item_id_to_item_name[mission.id + Constants.MISSION_COMPLETION_OFFSET] = mission.name
+
+
+# Reverse item_id_to_item_name
+item_name_to_item_id: typing.Dict[str, int] = {value: key for key, value in item_id_to_item_name.items()}
+
+class ACItem(Item):
+    game: str = Constants.GAME_NAME
+
+def create_item(name: str, player_id: int) -> ACItem:
+    return ACItem(name, ItemClassification.progression if (name in name_to_mission)
+                      else ItemClassification.filler, item_name_to_item_id[name], player_id)
+
+def create_victory_event(player_id: int) -> ACItem:
+    return ACItem(Constants.VICTORY_ITEM_NAME, ItemClassification.progression, Constants.VICTORY_ITEM_ID, player_id)
+
+def convert_item_id_to_mission_id(item_id: int) -> int:
+    return item_id - Constants.MISSION_COMPLETION_OFFSET
